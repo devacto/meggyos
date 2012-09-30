f3198ccebeb23a20925317ea3dd80e44995cf616
Claude Zhang <zbw0517@gmail.com>
2012-09-30 16:48:22 +1000

    Moved the script to /

--- ExamplesFromEric/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $# != 1 ]
-then
-    echo "Usage: compile.sh tone (not tone.c!)"
-else
-    avr-gcc -DF_CPU=16000000UL -mmcu=atmega328p -o $1.out $1.c
-    avr-objcopy -O ihex -R .eeprom $1.out $1.hex
-    avrdude -b57600 -patmega328p -cstk500v1 -P/dev/tty.usbserial-A501JU54 -U flash:w:$1.hex
-fi
