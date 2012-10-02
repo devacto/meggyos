df3e9f4813a94bcee4f47089a44d40aa6f8ad851
John <wengcsyz@gmail.com>
2012-10-02 17:32:28 +1000

    time interrupt

--- JohnDev/main.c
+++ JohnDev/main.c
@@ -7,9 +7,11 @@ extern uint8_t fbGreen[];
 extern uint8_t fbBlue[];
 extern uint8_t fbLights;
 
-int main()
+uint16_t i=0;
+
+main()
 {
-    uint16_t i=0, j=0;
+    
 
     turnOnFrameBuffer();
     fbLights = 0xAAU;
@@ -21,25 +23,35 @@ int main()
     fbRed[5] = 0x20U;
     fbRed[6] = 0x10U;
     fbRed[7] = 0xAAU;
-     
-    DDRB |= 1<<PB1; 
+    
+    DDRB |= 1<<PB1;   // set pin 15 to output
+   
+   TCCR2A = (1<<WGM21); // clear timer on compare match
+   TCCR2B = (1<<CS21);  // timer uses main system clock with 1/8 prescale
+   OCR2A  = (F_CPU >> 3) / 8 / 15 / 120; 
+   TIMSK2 = (1<<OCIE2A); // call interrupt on output compare match
+
+   sei( );    // Enable interrupts
 
     while(1)
     {
         drawFrameBuffer();
-        j++;
         
-        
-        
-        
-        
-
-        if(j>=100)
-        {
-            j=0;
-            fbLights =  (fbLights << 1) | (fbLights >> 7);
-            for (i =0; i < 8 ; i++)
-                fbRed[i] = (fbRed[i] << 1) | (fbRed[i] >> 7);
-        }
     }
 }
+
+
+ISR(TIMER2_COMPA_vect)
+{
+    i++;
+    if (i>2000)
+    {
+        fbLights =  (fbLights << 1) | (fbLights >> 7);
+        for (i =0; i < 8 ; i++)
+        {    
+            fbRed[i] = (fbRed[i] << 1) | (fbRed[i] >> 7);
+        }
+    }
+    
+}
+
