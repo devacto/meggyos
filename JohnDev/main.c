b4b51235642a0f5b351849cc50ed2e19e507b46c
John <wengcsyz@gmail.com>
2012-10-02 18:30:22 +1000

    push and pop

--- JohnDev/main.c
+++ JohnDev/main.c
@@ -9,6 +9,7 @@ extern uint8_t fbLights;
 
 uint16_t i=0,j=0,k=0;
 
+
 main()
 {
     
@@ -52,29 +53,92 @@ main()
 }
 
 
-ISR(TIMER0_COMPA_vect)
+ISR(TIMER0_COMPA_vect, ISR_NAKED)
 {
+    static uint16_t sph, spl;
+    asm("push r0");
+    asm("push r1");
+    asm("push r2");
+    asm("push r3");
+    asm("push r4");
+    asm("push r5");
+    asm("push r6");
+    asm("push r7");
+    asm("push r8");
+    sph = SPH;
+    spl = SPL;
+    
     sei();
     k++;
     if(k>100)
         fbGreen[1] = (fbGreen[1] << 1) | (fbGreen[1] >> 7);
-
+    
+    SPH = sph;
+    SPL = spl;
+    asm("pop r8");
+    asm("pop r7");
+    asm("pop r6");
+    asm("pop r5");
+    asm("pop r4");
+    asm("pop r3");
+    asm("pop r2");
+    asm("pop r1");
+    asm("pop r0");
+    reti();
 }
 
 
 
-ISR(TIMER1_COMPA_vect)
+ISR(TIMER1_COMPA_vect, ISR_NAKED)
 {
+    static uint16_t sph, spl;
+    asm("push r0");
+    asm("push r1");
+    asm("push r2");
+    asm("push r3");
+    asm("push r4");
+    asm("push r5");
+    asm("push r6");
+    asm("push r7");
+    asm("push r8");
+    sph = SPH;
+    spl = SPL;
+
     sei();
     j++;
     if(j>100)
         fbBlue[2] = (fbBlue[2] << 1) | (fbBlue[2] >> 7);
-
+    
+    SPH = sph;
+    SPL = spl;
+    asm("pop r8");
+    asm("pop r7");
+    asm("pop r6");
+    asm("pop r5");
+    asm("pop r4");
+    asm("pop r3");
+    asm("pop r2");
+    asm("pop r1");
+    asm("pop r0");
+    reti();
 }
 
 
-ISR(TIMER2_COMPA_vect)
+ISR(TIMER2_COMPA_vect, ISR_NAKED)
 {
+    static uint16_t sph, spl;
+    asm("push r0");
+    asm("push r1");
+    asm("push r2");
+    asm("push r3");
+    asm("push r4");
+    asm("push r5");
+    asm("push r6");
+    asm("push r7");
+    asm("push r8");
+    sph = SPH;
+    spl = SPL;
+
     sei();
     i++;
     if (i>2000)
@@ -85,7 +149,20 @@ ISR(TIMER2_COMPA_vect)
             fbRed[i] = (fbRed[i] << 1) | (fbRed[i] >> 7);
         }
     }
-    
+
+    SPH = sph;
+    SPL = spl;
+    asm("pop r8");
+    asm("pop r7");
+    asm("pop r6");
+    asm("pop r5");
+    asm("pop r4");
+    asm("pop r3");
+    asm("pop r2");
+    asm("pop r1");
+    asm("pop r0");
+
+    reti();
 }
 
 
